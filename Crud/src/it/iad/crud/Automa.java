8dfee72687a4ecacf682c0b0381f4223dc09abb2
irenedg <79708656+irenedg@users.noreply.github.com>
2021-03-12 16:47:45 +0100

    Commit 2

--- Crud/src/it/iad/crud/Automa.java
+++ Crud/src/it/iad/crud/Automa.java
@@ -1,18 +1,60 @@
-
 package it.iad.crud;
 
+public class Automa implements State {
 
-public class Automa implements State{
     private State stato;
 
-    public Automa() {       
+    public Automa() {
     }
-    
+
     @Override
     public void next(Event e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+
+    private class RicercaState implements State{
+
+        @Override
+        public void next(Event e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
     
+    private class VisualizzaState implements State{
+
+        @Override
+        public void next(Event e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
     
+    private class AggiungiState implements State{
+
+        @Override
+        public void next(Event e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+    
+    private class RimuoviState implements State{
+
+        @Override
+        public void next(Event e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+    
+    private class ModificaState implements State{
+
+        @Override
+        public void next(Event e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
 }
--- Crud/src/it/iad/crud/Crud.java
+++ Crud/src/it/iad/crud/Crud.java
@@ -3,7 +3,6 @@ package it.iad.crud;
 public class Crud {
 
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
